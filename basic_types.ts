69bd790033e00e9a879112a5563836a5b13afec5
Vyshali Prabananth Lal <69275275+bdpgmbv@users.noreply.github.com>
2021-03-16 13:36:29 -0400

    Update basic_types.ts

--- basic_types.ts
+++ basic_types.ts
@@ -151,7 +151,6 @@ function combine(input1: number | string, input2: number | string) {
 
 const combinedAges = combine(30, 26);
 console.log(combinedAges);
-
 const combinedNames = combine('Max', 'Anna');
 console.log(combinedNames);
 
@@ -174,9 +173,35 @@ function combine(
 
 const combinedAges = combine(30, 26, 'as-number');
 console.log(combinedAges);
-
 const combinedStringAges = combine('30', '26', 'as-number');
 console.log(combinedStringAges);
+const combinedNames = combine('Max', 'Anna', 'as-text');
+console.log(combinedNames);
 
+// ***************************************************************************************************************************************************************//
+// ALIASES usage
+// use the "type" keyword to create the aliases  
+
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
+
+function combine(
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionDescriptor
+) {
+  let result;
+  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+}
+
+const combinedAges = combine(30, 26, 'as-number');
+console.log(combinedAges);
+const combinedStringAges = combine('30', '26', 'as-number');
+console.log(combinedStringAges);
 const combinedNames = combine('Max', 'Anna', 'as-text');
 console.log(combinedNames);
