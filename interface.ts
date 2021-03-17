17b6e18a08dcb858962a5ee9c1965ac97566336a
Vyshali Prabananth Lal <69275275+bdpgmbv@users.noreply.github.com>
2021-03-17 13:23:26 -0400

    Update interface.ts

--- interface.ts
+++ interface.ts
@@ -18,3 +18,42 @@ user1 = {
 user1.greet('Hi there - I am');
 
 // ************************************************************************************************************************************************************** //
+
+interface Person {
+  readonly name: string;
+  age: number;
+
+  greet(phrase: string): void;
+}
+
+// or
+
+type Person {
+  readonly name: string;
+  age: number;
+
+  greet(phrase: string): void;
+}
+
+// ************************************************************************************************************************************************************** //
+
+// You can create function types with interfaces.
+
+interface AddFn { // TypeScript understands you want to use this interface as the function type and this is how the function should look like.
+  (a: number, b: number): number;
+} 
+
+let add: AddFn;
+
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
+// ************************************************************************************************************************************************************** //
+
+// OPTIONAL property
+
+interface Named {
+  readonly name: string;
+  outputName?: string; // this "?" tells TS this property might exist in class that implement this interface, but it does'nt have to. you can also mark methods as optional => "myMethod?(){...}"
+}
